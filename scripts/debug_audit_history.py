148286e01c41b4492daf9b49475577fd3d137f3e
rahulshirke7951 <rahulshirkegdrive@gmail.com>
2026-03-07 12:57:01 +0530

    Update debug_audit_history.py

--- scripts/debug_audit_history.py
+++ scripts/debug_audit_history.py
@@ -19,7 +19,7 @@ log = logging.getLogger(__name__)
 with open("config.json", "r") as f:
     config = json.load(f)
 
-ref_code = config.get("audit_scheme_code", 140088)
+ref_code = config.get("audit_scheme_code", 149758)
 return_periods = config.get("return_periods_days", [30, 365, 1095])
 log.info(f"--- 🔍 ROBUST MIRROR DEBUG START: Scheme {ref_code} ---")
 
